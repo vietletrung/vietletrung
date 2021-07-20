81fcb0e7366b57d82a10aa9ad68de14e444139b9
viet.le <viet.le@asia.thalesgroup.com>
2021-07-20 08:37:34 +0800

    add travis aws

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          good luck
+          good luck 2
         </p>
         <a
           className="App-link"
